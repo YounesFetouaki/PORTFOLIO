c0a466443e9850fbfe92be728452ec11ce610953
YounesFetouaki <104641370+YounesFetouaki@users.noreply.github.com>
2025-01-21 14:40:05 +0100

    Update mode-toggle.jsx

--- portfolio/src/components/mode-toggle.jsx
+++ portfolio/src/components/mode-toggle.jsx
@@ -6,7 +6,7 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+} from "@/components/ui/dropdown-menu"; 
 import { useTheme } from "@/components/theme-provider";
 
 export function ModeToggle() {
